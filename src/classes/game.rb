0869b59f2f6edeaf71f9e1c976f9cbd29963aab9
Oluyemi Paul <oluola96@gmail,com>
2022-11-15 16:24:48 +0100

    create can_be_archieved? method

--- src/classes/game.rb
+++ src/classes/game.rb
@@ -1,3 +1,18 @@
+require_relative '../item'
+
 # Create Game class
-class Game
-end
+class Game < Item
+    attr_accessor :multiplayer, :last_played_at, :name
+  
+    def initialize(publish_date, multiplayer, last_played_at, name)
+      super(publish_date)
+      @multiplayer = multiplayer
+      @last_played_at = last_played_at
+      @name = name
+    end
+  
+    def can_be_archieved?
+      duration = Date.today.year - @last_played_at.year
+      super && duration > 2
+    end
+  end
