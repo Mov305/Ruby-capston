2eff49dc2b2d0887d2d4cbecb4b6906925e631ad
ogiogiovictor@gmail.com <ogiogiovictor@gmail.com>
2022-11-15 06:12:29 -0600

    fixed music album issues

--- src/music_album.rb
+++ src/music_album.rb
@@ -1,15 +1,14 @@
 class MusicAlbum
+  attr_accessor :on_spotify, :title
+  attr_reader :archived
 
-    attr_accessor :on_spotify, :title
+  def initialize(on_spotify, title, archived: false)
+    @on_spotify = on_spotify
+    @title = title
+    @archived = archived
+  end
 
-    def initialize(on_spotify, title, archived: false)
-        @on_spotify = on_spotify
-        @title = title
-      end
-    
-      def can_be_archived?
-         @on_spotify == 'Yes'
-      end
-
-
+  def can_be_archived?
+    @on_spotify == 'Yes'
+  end
 end
