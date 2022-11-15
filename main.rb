ea99a342ad314858aca6b86ec26c94d0ee51896f
ogiogiovictor@gmail.com <ogiogiovictor@gmail.com>
2022-11-15 01:54:55 -0600

    Added genre to list of options

--- main.rb
+++ main.rb
@@ -3,14 +3,15 @@ require "./src/processor"
 def present_options
   puts "Welcome to the Catalogue! 
   
-  
   Choose an option: 
-  
   1. List Items
-  2. Add a new Book 
+  2. Add a new Book 6
   3. Add a new Music album 
   4. Add a new Movie 
   5. Add a new Game 
+  6. List all music albums
+  7. List all genres 
+  8. Add a music album
   6. Exit"
 end
 
