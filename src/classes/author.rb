d5f287a305cad861adb6bb52b7ad2a094cf1c7ab
Oluyemi Paul <oluola96@gmail,com>
2022-11-15 16:27:14 +0100

    cerate add_item method

--- src/classes/author.rb
+++ src/classes/author.rb
@@ -1,5 +1,18 @@
+require 'securerandom'
 require_relative '../item'
 
 # Create an Author class with an association to the Item class
-class Author < Item
+class Author
+  attr_accessor :id, :name, :items
+
+  def initialize(name)
+    @id = SecureRandom.uuid
+    @name = name
+    @items = []
+  end
+
+  def add_item(item)
+    @items << item unless @items.include?(item)
+    item.add_genre(self)
+  end
 end
