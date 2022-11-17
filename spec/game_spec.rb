262b553b698618f3483d4e17211676a7c22c2620
Oluyemi Paul <oluola96@gmail,com>
2022-11-17 13:10:37 +0100

    add test for game class

---
+++ spec/game_spec.rb
@@ -0,0 +1,30 @@
+require_relative '../Classes/game'
+require_relative '../Classes/item'
+
+RSpec.describe Game do
+  it 'should be a game' do
+    game = Game.new('2006-06-03', 'Barbra', '2009-08-09', 'Barbie')
+    expect(game.class).to eq(Game)
+  end
+
+  it 'should have a name' do
+    game = Game.new('2006-06-03', 'Barbra', '2009-08-09', 'Barbie')
+    expect(game.name).to eq('Barbie')
+  end
+
+  it ' should have a publish date' do
+    game = Game.new('2006-06-03', 'Barbra', '2009-08-09', 'Barbie')
+    expect(game.publish_date).to eq(Date.parse('2006-06-03'))
+  end
+
+  it 'should have a last_played_at date' do
+    game = Game.new('2006-06-03', 'Barbra', '2009-08-09', 'Barbie')
+    expect(game.last_played_at).to eq('2009-08-09')
+  end
+
+  it 'should be able to be archived' do
+    game = Game.new('2006-06-03', 'Barbra', '2009-08-09', 'Barbie')
+    game.move_to_archive
+    expect(game.archived).to eq(true)
+  end
+end
